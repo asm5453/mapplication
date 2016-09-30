cbf9f390a8609cc8cf96cd77028ce7f7e2884e5b
asm5453 <amoy002@gmail.com>
2016-09-30 15:38:07 -0400

    Initial Commit

---
+++ Mapplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mapplication
+//
+//  Created by Adam Moyer on 9/30/16.
+//  Copyright © 2016 Adam Moyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
